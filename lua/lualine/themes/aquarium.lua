fbc4aeb7e301dfbcd32d6d8e107e4d6c8e688360
genmzy <986299679@qq.com>
2022-04-02 23:19:36 +0800

    change: color fg of mode.x.b

--- lua/lualine/themes/aquarium.lua
+++ lua/lualine/themes/aquarium.lua
@@ -2,27 +2,27 @@ local colors = vim.fn['aquarium#colors#definitions']()
 return {
     normal = {
         a = {bg = colors.gui02, fg = colors.gui03, gui = colors.bold},
-        b = {bg = colors.gui01, fg = colors.gui04},
+        b = {bg = colors.gui01, fg = colors.gui0E},
         c = {bg = colors.gui03, fg = colors.gui04}
     },
     insert = {
         a = {bg = colors.gui0F, fg = colors.gui03, gui = colors.bold},
-        b = {bg = colors.gui01, fg = colors.gui04},
+        b = {bg = colors.gui01, fg = colors.gui0E},
         c = {bg = colors.gui03, fg = colors.gui04}
     },
     visual = {
         a = {bg = colors.gui03, fg = colors.gui04, gui = colors.bold},
-        b = {bg = colors.gui01, fg = colors.gui04},
+        b = {bg = colors.gui01, fg = colors.gui0E},
         c = {bg = colors.gui03, fg = colors.gui04}
     },
     replace = {
         a = {bg = colors.gui03, fg = colors.gui04, gui = colors.bold},
-        b = {bg = colors.gui01, fg = colors.gui04},
+        b = {bg = colors.gui01, fg = colors.gui0E},
         c = {bg = colors.gui03, fg = colors.gui04}
     },
     command = {
         a = {bg = colors.gui0E, fg = colors.gui03, gui = colors.bold},
-        b = {bg = colors.gui01, fg = colors.gui04},
+        b = {bg = colors.gui01, fg = colors.gui0E},
         c = {bg = colors.gui03, fg = colors.gui04}
     },
     inactive = {
